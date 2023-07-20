479e4a56d68a978812d9b7ca91e49695690e3154
N1ULL <34934155+N1ULL@users.noreply.github.com>
2023-07-20 08:36:33 +0800

    更新 ex4.bat

--- Day3/ex4.bat
+++ Day3/ex4.bat
@@ -8,7 +8,7 @@ gmtset LABEL_FONT_SIZE 17P
 
 grdcut topo30.grd -R80/90/40/50 -Gnew1.grd
 grdgradient new1.grd -A90 -Ne0.8 -M -Gnew1.grd.int
-grdimage new1.grd -R -JM10 -Ba5f1g1/a5f1g1:."天山地形图":neWS -Inew1.grd.int -CGMT_globe.cpt -Xc -Y3 -K>plot3.ps
+grdimage new1.grd -R -JM10 -Ba5f1g1/a5f1g1:."澶╁北鍦板舰":neWS -Inew1.grd.int -CGMT_globe.cpt -Xc -Y3 -K>plot3.ps
 pscoast -R -J -N1/0.5p,black -W1/1p,blue -O -K >> plot3.ps
-psscale -CGMT_globe.cpt -D5/-1/10/0.2h -B3500:"高程":/1000:"m": -O>>plot3.ps
+psscale -CGMT_globe.cpt -D5/-1/10/0.2h -B3500:"楂樼▼":/1000:"m": -O>>plot3.ps
 ps2raster -C-sFONTPATH="C:\Windows\Fonts" plot3.ps -Tg -P

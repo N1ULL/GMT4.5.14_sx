2bf68d7298cb5224a2e03e70836964925426a82d
N1ULL <34934155+N1ULL@users.noreply.github.com>
2023-07-20 08:37:28 +0800

    更新 ex1.bat

--- Day3/ex1.bat
+++ Day3/ex1.bat
@@ -3,10 +3,10 @@ gmtset HEADER_FONT_SIZE 18P
 gmtset HEADER_FONT 38
 gawk "{if($4>4) print $2,$1}" China_earthquake.txt >new1.txt
 gawk "{if($4<4 && $4>2) print $2,$1}" China_earthquake.txt >new2.txt
-psbasemap -R70/135/10/55 -JM12 -Ba10f5/a10f5:."ÖÐ¹úµØÍ¼": -Xc -Y1 -P -K> plot.ps
+psbasemap -R70/135/10/55 -JM12 -Ba10f5/a10f5:."ä¸­å›½åœ°å½¢å›¾": -Xc -Y1 -P -K> plot.ps
 pscoast -R70/135/10/55 -JM12 -N1/0.5p,black -W1/0.5p,blue -I1/0.5p,gold -SROYALBLUE -G255/185/230 -CROYALBLUE -O -K >> plot.ps
 psxy -R70/135/10/55 -JM12 new1.txt -Sc0.05 -W0.05p/255/0/0 -G0/0/0 -M -O -K>>plot.ps
-psbasemap -R70/135/10/55 -JM12 -Ba10f5/a10f5:."ÖÐ¹úµØÍ¼": -Y15 -P -K -O>> plot.ps
+psbasemap -R70/135/10/55 -JM12 -Ba10f5/a10f5:."é«˜ç¨‹": -Y15 -P -K -O>> plot.ps
 pscoast -R70/135/10/55 -JM12 -N1/0.5p,black -W1/0.5p,blue -I1/0.5p,gold -SROYALBLUE -G255/185/230 -CROYALBLUE -O -K >> plot.ps
 psxy -R70/135/10/55 -JM12 new2.txt -Sc0.05 -W0.05p/255/0/0 -G0/0/0 -M -O >>plot.ps
 ps2raster -C-sFONTPATH="C:\Windows\Fonts" plot.ps -Tg -P

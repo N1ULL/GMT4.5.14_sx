6c1c4091eb5bb580619aadabbc874eb048278a73
N1ULL <34934155+N1ULL@users.noreply.github.com>
2023-07-20 22:45:55 +0800

    Add files via upload

--- Day4/ex5.bat
+++ Day4/ex5.bat
@@ -16,10 +16,10 @@ pscoast -R -J -N1/0.5p,black -W1/1p,blue -O -K >> plot4.ps
 
 rem 改速度条
 surface Z_vp0.txt -R -I1m -Ll4.0 -Lu6.0 -GZ_vp0.grd
-grd2cpt Z_vp0.grd -CGMT_seis.cpt -S4.3/5.2/0.1 -I >Z_vp0.cpt
+grd2cpt Z_vp0.grd -CGMT_seis.cpt -S4.3/5.2/0.01 -I >Z_vp0.cpt
 grdimage Z_vp0.grd -R -J -B20/20 -CZ_vp0.cpt -Inew2.grd.int -O -K >> plot4.ps
 
 psscale -CZ_vp0.cpt -D8/-1/16/0.2h -B0.2:"P波速度":/0.2:"m/s": -O>>plot4.ps
 
 
-rem ps2raster -C-sFONTPATH="C:\Windows\Fonts" plot4.ps -Tg -P
+ps2raster -C-sFONTPATH="C:\Windows\Fonts" plot4.ps -Tg -P
